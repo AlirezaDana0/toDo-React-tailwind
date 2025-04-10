bfd6786f9479a342d87a99bf790e5f32d0f0e2c4
Alireza Dana <alidanadev@gmail.com>
2025-04-10 22:00:17 +0330

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
     <div>
-        <Link to="./Countries" className='p-2 bg-green-300 rounded-lg my-2'> Countries </Link>
         
     </div>
   )
