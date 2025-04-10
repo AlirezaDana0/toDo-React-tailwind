b8caa789b5d46707171825a06117524a17592030
Alireza Dana <alidanadev@gmail.com>
2025-04-10 22:00:59 +0330

    Update Layout.jsx

--- src/pages/Layouts/Layout.jsx
+++ src/pages/Layouts/Layout.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+
 
 export default function Layout() {
   return (
     <div>
         <ul>
             <li>
-                <NavLink to="./Countries"></NavLink>
+                
             </li>
         </ul>
     </div>
